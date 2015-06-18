a76c0dd44d0da78ed3e524676b00400ed535c9d7
vasudev bhat <vasudevbbhat@gmail.com>
2015-06-18 21:31:13 +0530

    new

--- main.php
+++ main.php
@@ -6,8 +6,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="css/style.min.css" />
 		<script src="js/jquery-1.10.2.min.js"></script>
-		<script src="js/jquery.mobile-1.3.2.min.js"></script>
-        <script src="js/modernizr.js"></script>
+		
+    
         
 	<style>
 	html,body{overflow-y:hidden}
@@ -65,29 +65,27 @@ a.styled-button-1:hover{
 	</head>
 	<body>
  
-		<!-- Home Page -->
-		<div data-role="page" id="home" data-theme="a" data-transition="flip">
-			<div data-role="header"  class="custom-header" data-tap-toggle="false" data-id="header" >
-				 
-				<div class="logo"><a href="javascript:window.history.back()" data-direction="reverse" class="styled-button-1" data-theme="a" data-icon="back">Back</a></div>
-			</div>
-			<div data-role="content" style="height:100vh">
-				<div class="quote" id="content" >
-			 			 	 
-					 
-                    <iframe class="frame" frameborder="0" src="<?=$_GET['url']?>"></iframe>
-                    
-				</div>
-			 
-			</div>
-		</div>
-			<!-- end of home -->
+ <!-- magazine Page -->
+<div data-role="page" id="magazine" data-theme="a" >
+           <div data-role="header"  class="custom-header" data-tap-toggle="false" data-id="header" >
+   
+  </div>
+           <div data-role="content">
+     	<iframe class="frame" frameborder="0" src="<?=$_GET['url']?>"></iframe>
+    <div class="clear"></div>
+  </div>
+           <div class="custom-footer"> 
+    <!-- footer --> 
+    <a href="javascript:window.history.back()" data-transition="flip"><img src="img/home.png" border="0"></a><a href="#" onclick="window.open('https://www.facebook.com/pages/WOGOA-Magazine/430510810418410','_system','location=yes')"><img src="img/facebook.png" border="0"></a><a  onclick="window.open('https://twitter.com/WOGOAMAGAZINE','_system','location=yes')" href="#"><img id="openTwitter" src="img/twitter.png" border="0"></a> </div>
+         </div>
+
+<!-- end of magazine --> 
  
 	</body>
 	 
-	
+	<script src="js/jquery.mobile-1.3.2.min.js"></script>
     <script src="phonegap.js"></script>
-    <script src="cordova.js"></script>
+ 
 		
 		
 </html>
